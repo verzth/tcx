49d5ef72132c7d8a8fc36dbb2cc836acf92b2029
Dodi <dodi@ampsecuretech.com>
2019-07-04 16:39:50 +0700

    change class name

--- database/migrations/20190704_04_00_create_tcx_applications_table.php
+++ database/migrations/20190704_04_00_create_tcx_applications_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class TCXApplicationsTable201907040400 extends Migration{
+class TCXApplicationsTable extends Migration{
     private static $table = "tcx_applications";
     public function up(){
         Schema::table(self::$table,function (Blueprint $table){
